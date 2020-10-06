6dc5f4919094900a574278775967c6a857c0457d
caglar7 <mcaglarcnkr@gmail.com>
2020-10-06 23:12:40 +0300

    setting up beacon manager and consumer

--- app/src/main/java/com/example/beacon_project/MainActivity.java
+++ app/src/main/java/com/example/beacon_project/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.beacon_project;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
 
+public class MainActivity extends AppCompatActivity implements BeaconConsumer{
+    private static final String TAG = "MainActivity";
+
+    private BeaconManager beaconManager = null;
+
+    private static final String ALTBEACON_LAYOUT = "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";
+
+
+    // this call here just before the onCreate method
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermissions(new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, 1234);
+
+        // for beaconManager singleton
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        // look for altbeacons for now
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(ALTBEACON_LAYOUT));
+
+        // bind beaconManager to this activity
+        beaconManager.bind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Log.d(TAG, "onBeaconServiceConnect is called");
+
+        beaconManager.addMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+
+            }
+        });
     }
 }
